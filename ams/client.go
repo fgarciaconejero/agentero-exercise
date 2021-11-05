48829361224a9b920319580ce1836e522de04133
Facundo Garcia Conejero <fgarciaconejero@gmail.com>
2021-11-05 12:15:32 +0100

    Copied the data from the google document to use as mocks

--- ams/client.go
+++ ams/client.go
@@ -30,5 +30,66 @@ func handleRequests() {
 	}
 }
 
-var users = []policy_holder_pb.PolicyHolder{}
-var policies = []policy_holder_pb.InsurancePolicy{}
+var users = []policy_holder_pb.PolicyHolder{
+	{
+		Name:         "user1",
+		MobileNumber: "1234567890",
+	},
+	{
+		Name:         "user2",
+		MobileNumber: "123 456 7891",
+	},
+	{
+		Name:         "user3",
+		MobileNumber: "(123) 456 7892",
+	},
+	{
+		Name:         "user4",
+		MobileNumber: "(123) 456-7893",
+	},
+	{
+		Name:         "user5",
+		MobileNumber: "123-456-7894",
+	}}
+var policies = []policy_holder_pb.InsurancePolicy{
+	{
+		MobileNumber: "1234567890",
+		Premium:      2000,
+		Type:         "homeowner",
+	},
+	{
+		MobileNumber: "123 456 7891",
+		Premium:      200,
+		Type:         "renter",
+	},
+	{
+		MobileNumber: "123-456-7892",
+		Premium:      1500,
+		Type:         "homeowner",
+	},
+	{
+		MobileNumber: "(123) 456-7893",
+		Premium:      155,
+		Type:         "personal_auto",
+	},
+	{
+		MobileNumber: "123-456-7894",
+		Premium:      1000,
+		Type:         "homeowner",
+	},
+	{
+		MobileNumber: "123-456-7890",
+		Premium:      500,
+		Type:         "personal_auto",
+	},
+	{
+		MobileNumber: "1234567892",
+		Premium:      100,
+		Type:         "life",
+	},
+	{
+		MobileNumber: "(123)456-7892",
+		Premium:      200,
+		Type:         "homeowner",
+	},
+}
