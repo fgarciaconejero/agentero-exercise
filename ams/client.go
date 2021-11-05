2a4b1f5ed41bfec8528b0c9aafb72596c103d129
Facundo Garcia Conejero <fgarciaconejero@gmail.com>
2021-11-05 12:12:08 +0100

    Adding exposed endpoints and mock responses to the Fake AMS API in order to be able to test all the servers and clients together via Postman

--- ams/client.go
+++ ams/client.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"log"
+	"net/http"
 
+	"github.com/agentero-go/policy_holder/policy_holder_pb"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,7 +15,20 @@ func main() {
 func handleRequests() {
 	g := gin.Default()
 
+	g.GET("/users/:agentid", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, &users)
+	})
+
+	g.GET("/policies/:agentId", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"policy_holder": policies,
+		})
+	})
+
 	if err := g.Run("localhost:8081"); err != nil {
 		log.Fatalf("Failed to run server: %v", err)
 	}
 }
+
+var users = []policy_holder_pb.PolicyHolder{}
+var policies = []policy_holder_pb.InsurancePolicy{}
