e216f9f05205d87aba16caf830dd3250e24b2de4
Facundo Garcia Conejero <fgarciaconejero@gmail.com>
2021-11-05 17:43:09 +0100

    Adding interface to be able to make the service mock work

---
+++ agentero/service/service_interface.go
@@ -0,0 +1,8 @@
+package service
+
+import "github.com/agentero-exercise/agentero/resources/protos"
+
+type IService interface {
+	GetPolicyHoldersFromAms(string) ([]*protos.PolicyHolder, error)
+	GetInsurancePoliciesFromAms(string) ([]*protos.InsurancePolicy, error)
+}
