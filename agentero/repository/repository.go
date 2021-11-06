fe68a1067e2fce803d6611f15b5c15e88f918b00
Facundo Garcia Conejero <fgarciaconejero@gmail.com>
2021-11-06 14:38:59 +0100

    Setting up database on repository start up

--- agentero/repository/repository.go
+++ agentero/repository/repository.go
@@ -1,8 +1,24 @@
 package repository
 
-import "github.com/agentero-exercise/agentero/resources/protos"
+import (
+	"database/sql"
+
+	"github.com/agentero-exercise/agentero/resources/protos"
+)
 
 type Repository struct {
+	db sql.DB
+}
+
+func NewRepository() (*Repository, error) {
+	db, err := SetDatabaseUp()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Repository{
+		db: *db,
+	}, nil
 }
 
 func (*Repository) GetById(agentId string) ([]*protos.PolicyHolder, error) {
@@ -15,4 +31,33 @@ func (*Repository) GetByMobileNumber(agentId string) ([]protos.PolicyHolder, err
 
 func (*Repository) Upsert(phs []*protos.PolicyHolder) error {
 	return nil
+}
+func SetDatabaseUp() (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", "file::memory:?cache=shared")
+	if err != nil {
+		return nil, err
+	}
+
+	// Asking if the tables are already created so that we don't have a duplicate table error
+
+	var count int
+	err = db.QueryRow("SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name=?", "policy_holders").Scan(&count)
+	if err != nil {
+		return nil, err
+	}
+
+	if count == 0 {
+		_, err = db.Exec("CREATE TABLE `policy_holders`" +
+			"(`id_ph` integer, PRIMARY KEY `id_ph`)")
+		if err != nil {
+			return nil, err
+		}
+
+		_, err = db.Exec("CREATE UNIQUE INDEX `id_ph_UNIQUE` ON `users`(`id_ph`)")
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return db, nil
 }
