ec2cb3af9c08e8176d75f865a36915203252481e
Facundo Garcia Conejero <fgarciaconejero@gmail.com>
2021-11-06 17:34:34 +0100

    Added GetAllInsuranceAgentsIds to interface of service

--- agentero/service/service_interface.go
+++ agentero/service/service_interface.go
@@ -5,5 +5,6 @@ import "github.com/agentero-exercise/agentero/resources/protos"
 type IService interface {
 	GetPolicyHoldersFromAms(string) ([]*protos.PolicyHolder, error)
 	GetInsurancePoliciesFromAms(string) ([]*protos.InsurancePolicy, error)
+	GetAllInsuranceAgentsIds() ([]string, error)
 	UpsertPolicyHoldersAndInsurancePoliciesIntoSQLite([]*protos.PolicyHolder) error
 }
