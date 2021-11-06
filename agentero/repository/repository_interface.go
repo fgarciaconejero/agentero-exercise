722087c0fd84c0a8bff493f25d0ce9e5e4580d90
Facundo Garcia Conejero <fgarciaconejero@gmail.com>
2021-11-06 14:28:17 +0100

    Refactored and added get methods

--- agentero/repository/repository_interface.go
+++ agentero/repository/repository_interface.go
@@ -4,6 +4,7 @@ import "github.com/agentero-exercise/agentero/resources/protos"
 
 type IRepository interface {
 	// GetPolicyHoldersFromAms(string) ([]*protos.PolicyHolder, error)
-	Get(string) (*protos.PolicyHolder, error)
+	GetById(string) ([]*protos.PolicyHolder, error)
+	GetByMobileNumber(string) (*protos.PolicyHolder, error)
 	Upsert(*protos.PolicyHolder) error
 }
