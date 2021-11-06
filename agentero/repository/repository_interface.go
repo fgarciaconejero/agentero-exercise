7b659acc8f4408b8aef505fb64ca75d686635a48
Facundo Garcia Conejero <fgarciaconejero@gmail.com>
2021-11-06 14:58:37 +0100

    Removing unnecessary comment

--- agentero/repository/repository_interface.go
+++ agentero/repository/repository_interface.go
@@ -3,7 +3,6 @@ package repository
 import "github.com/agentero-exercise/agentero/resources/protos"
 
 type IRepository interface {
-	// GetPolicyHoldersFromAms(string) ([]*protos.PolicyHolder, error)
 	GetById(string) ([]*protos.PolicyHolder, error)
 	GetByMobileNumber(string) (*protos.PolicyHolder, error)
 	Upsert([]*protos.PolicyHolder) error
