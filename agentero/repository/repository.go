c83992bd30433a405b61af9b35bf3be1ac0a9631
Facundo Garcia Conejero <fgarciaconejero@gmail.com>
2021-11-06 14:43:40 +0100

    Adding missing table to db and columns to existing table

--- agentero/repository/repository.go
+++ agentero/repository/repository.go
@@ -41,19 +41,30 @@ func SetDatabaseUp() (*sql.DB, error) {
 	// Asking if the tables are already created so that we don't have a duplicate table error
 
 	var count int
-	err = db.QueryRow("SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name=?", "policy_holders").Scan(&count)
+	err = db.QueryRow("SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name=?", "policy_holders", "insurance_policies").Scan(&count)
 	if err != nil {
 		return nil, err
 	}
 
 	if count == 0 {
 		_, err = db.Exec("CREATE TABLE `policy_holders`" +
-			"(`id_ph` integer, PRIMARY KEY `id_ph`)")
+			"(`id_ph` integer, `name` string, `mobile_number` string, PRIMARY KEY `id_ph`)")
 		if err != nil {
 			return nil, err
 		}
 
 		_, err = db.Exec("CREATE UNIQUE INDEX `id_ph_UNIQUE` ON `users`(`id_ph`)")
+		if err != nil {
+			return nil, err
+		}
+
+		_, err = db.Exec("CREATE TABLE `insurance_policies`" +
+			"(`id_ip` integer, `mobile_number` string, `premium` integer, `type` string, PRIMARY KEY `id_ip`)")
+		if err != nil {
+			return nil, err
+		}
+
+		_, err = db.Exec("CREATE UNIQUE INDEX `id_ip_UNIQUE` ON `users`(`id_ip`)")
 		if err != nil {
 			return nil, err
 		}
