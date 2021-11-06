317057de9cb4ac0028fe3598b4034ad99a045c5b
Facundo Garcia Conejero <fgarciaconejero@gmail.com>
2021-11-06 17:47:13 +0100

    Added reminder TODO and GetPolicyHoldersFromSQLite to interface

--- agentero/service/service_interface.go
+++ agentero/service/service_interface.go
@@ -5,6 +5,8 @@ import "github.com/agentero-exercise/agentero/resources/protos"
 type IService interface {
 	GetPolicyHoldersFromAms(string) ([]*protos.PolicyHolder, error)
 	GetInsurancePoliciesFromAms(string) ([]*protos.InsurancePolicy, error)
+	GetPolicyHoldersFromSQLite(string) ([]*protos.PolicyHolder, error)
+	// TODO: GetInsurancePoliciesFromSQLite
 	GetAllInsuranceAgentsIds() ([]string, error)
 	UpsertPolicyHoldersAndInsurancePoliciesIntoSQLite([]*protos.PolicyHolder) error
 }
