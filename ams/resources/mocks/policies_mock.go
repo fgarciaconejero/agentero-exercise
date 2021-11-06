b6b52cc3668dc88367f7d6849b15ff0342588fec
Facundo Garcia Conejero <fgarciaconejero@gmail.com>
2021-11-06 23:14:36 +0100

    Mapping some of the mock policies to the agents

--- ams/resources/mocks/policies_mock.go
+++ ams/resources/mocks/policies_mock.go
@@ -7,40 +7,48 @@ var Policies = []protos.InsurancePolicy{
 		MobileNumber: "1234567890",
 		Premium:      2000,
 		Type:         "homeowner",
+		AgentId:      "1",
 	},
 	{
 		MobileNumber: "123 456 7891",
 		Premium:      200,
 		Type:         "renter",
+		AgentId:      "2",
 	},
 	{
 		MobileNumber: "123-456-7892",
 		Premium:      1500,
 		Type:         "homeowner",
+		AgentId:      "1",
 	},
 	{
 		MobileNumber: "(123) 456-7893",
 		Premium:      155,
 		Type:         "personal_auto",
+		AgentId:      "3",
 	},
 	{
 		MobileNumber: "123-456-7894",
 		Premium:      1000,
 		Type:         "homeowner",
+		AgentId:      "1",
 	},
 	{
 		MobileNumber: "123-456-7890",
 		Premium:      500,
 		Type:         "personal_auto",
+		AgentId:      "1",
 	},
 	{
 		MobileNumber: "1234567892",
 		Premium:      100,
 		Type:         "life",
+		AgentId:      "2",
 	},
 	{
 		MobileNumber: "(123)456-7892",
 		Premium:      200,
 		Type:         "homeowner",
+		AgentId:      "1",
 	},
 }
