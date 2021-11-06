76806b5e3093fcff9edee907d0e7281096bbaab8
Facundo Garcia Conejero <fgarciaconejero@gmail.com>
2021-11-06 23:15:28 +0100

    Adding agents mocks to agentero

---
+++ agentero/resources/mocks/agents_mock.go
@@ -0,0 +1,18 @@
+package mocks
+
+import "github.com/agentero-exercise/agentero/domain/models"
+
+var Agents = []models.Agent{
+	{
+		Id:   "1",
+		Name: "John Fakelastname",
+	},
+	{
+		Id:   "2",
+		Name: "Mike Fakelastname",
+	},
+	{
+		Id:   "3",
+		Name: "Antony Fakelastname",
+	},
+}
