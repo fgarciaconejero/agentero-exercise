a48659dcd33a1ce6bd899512c664f2ff4e6d09c7
Facundo Garcia Conejero <fgarciaconejero@gmail.com>
2021-11-07 12:03:48 +0100

    Added filter functionality to the Fake AMS API client

--- ams/client.go
+++ ams/client.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/agentero-exercise/agentero/resources/protos"
 	"github.com/agentero-exercise/ams/resources/mocks"
 	"github.com/gin-gonic/gin"
 )
@@ -16,12 +18,12 @@ func handleRequests() {
 	g := gin.Default()
 
 	// TODO: Make it so that this endpoint and the one below filter out by agentId
-	g.GET("/users/:agentid", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, mocks.Users)
+	g.GET("/users/:agentId", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, returnUsersByAgentId(ctx.Param("agentId")))
 	})
 
 	g.GET("/policies/:agentId", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, mocks.Policies)
+		ctx.JSON(http.StatusOK, returnPoliciesByAgentId(ctx.Param("agentId")))
 	})
 
 	g.GET("/agents", func(ctx *gin.Context) {
@@ -31,4 +33,29 @@ func handleRequests() {
 	if err := g.Run("localhost:8081"); err != nil {
 		log.Fatalf("Failed to run server: %v", err)
 	}
+}
+
+func returnPoliciesByAgentId(agentId string) (ips []*protos.InsurancePolicy) {
+	// This for range is not done in the logical 'for i, v :=...' way because lint throws a warning
+	for i := range mocks.Policies {
+		if mocks.Policies[i].AgentId == agentId {
+			ips = append(ips, &mocks.Policies[i])
+		}
+	}
+	return
+}
+
+func returnUsersByAgentId(agentId string) (phs []*protos.PolicyHolder) {
+	// This for range is not done in the logical 'for i, v :=...' way because lint throws a warning
+	for i := range mocks.Users {
+		for x := range mocks.Policies {
+			if (mocks.Policies[x].MobileNumber == mocks.Users[i].MobileNumber) && (mocks.Policies[x].AgentId == agentId) {
+				phs = append(phs, &mocks.Users[i])
+				break
+			}
+		}
+	}
+	fmt.Println("PHS:", phs)
+	fmt.Println("PHS lens:", len(phs))
+	return
 }
