3111eff911e35e2deedb626a6d5fce5e781eb3d2
Facundo Garcia Conejero <fgarciaconejero@gmail.com>
2021-11-07 12:45:22 +0100

    Added mobile number formatting

--- ams/client.go
+++ ams/client.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"regexp"
 
 	"github.com/agentero-exercise/agentero/resources/protos"
 	"github.com/agentero-exercise/ams/resources/mocks"
@@ -33,25 +35,39 @@ func handleRequests() {
 	}
 }
 
+func compileRegexp() (reg *regexp.Regexp) {
+	// This regexp filters everything but numbers out
+	reg, _ = regexp.Compile("[^0-9]+")
+	return
+}
+
 func returnPoliciesByAgentId(agentId string) (ips []*protos.InsurancePolicy) {
+	reg := compileRegexp()
+
 	// This for range is not done in the logical 'for i, v :=...' way because lint throws a warning
-	for i := range mocks.Policies {
-		if mocks.Policies[i].AgentId == agentId {
-			ips = append(ips, &mocks.Policies[i])
+	for _, v := range mocks.Policies {
+		if v.AgentId == agentId {
+			v.MobileNumber = reg.ReplaceAllString(v.MobileNumber, "")
+			ips = append(ips, &v)
 		}
 	}
 	return
 }
 
 func returnUsersByAgentId(agentId string) (phs []*protos.PolicyHolder) {
+	reg := compileRegexp()
+	fmt.Println("Here?")
+
 	// This for range is not done in the logical 'for i, v :=...' way because lint throws a warning
-	for i := range mocks.Users {
-		for x := range mocks.Policies {
-			if (mocks.Policies[x].MobileNumber == mocks.Users[i].MobileNumber) && (mocks.Policies[x].AgentId == agentId) {
-				phs = append(phs, &mocks.Users[i])
+	for _, v := range mocks.Users {
+		for _, x := range mocks.Policies {
+			if (x.MobileNumber == v.MobileNumber) && (x.AgentId == agentId) {
+				v.MobileNumber = reg.ReplaceAllString(v.MobileNumber, "")
+				phs = append(phs, &v)
 				break
 			}
 		}
 	}
+	fmt.Println("Here!")
 	return
 }
