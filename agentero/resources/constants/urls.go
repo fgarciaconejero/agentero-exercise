99d37f39e691b5cd16146aab6aa8a8aa07dbd463
Facundo Garcia Conejero <fgarciaconejero@gmail.com>
2021-11-08 16:22:54 +0100

    Fixed bug

--- agentero/resources/constants/urls.go
+++ agentero/resources/constants/urls.go
@@ -1,5 +1,5 @@
 package constants
 
-const ClientUrl string = "http://localhost:8080"
+const ClientUrl string = "localhost:8080"
 const ServerUrl string = "http://localhost:50051"
 const AmsClientUrl string = "http://localhost:8081"
