3d4c2fdb5379b02ac01e7d1a5c797a65eeeeac2f
Facundo Garcia Conejero <fgarciaconejero@gmail.com>
2021-11-08 17:26:12 +0100

    Adding Mac specific constants

--- agentero/resources/constants/urls.go
+++ agentero/resources/constants/urls.go
@@ -1,5 +1,11 @@
 package constants
 
+// Windows url constants
 const ClientUrl string = "localhost:8080"
-const ServerUrl string = "http://localhost:50051"
+const ServerUrl string = "localhost:50051"
 const AmsClientUrl string = "http://localhost:8081"
+
+// Mac url constants
+// const ClientUrl string = "127.0.0.1:8080"
+// const ServerUrl string = "127.0.0.1:50051"
+// const AmsClientUrl string = "http://127.0.0.1:8081"
